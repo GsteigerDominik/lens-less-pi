21b36feab9c1ff6cbeb10ba99c62e4d278922ed3
GsteigerDominik <dominik.gsteiger@gmail.com>
2024-06-04 21:10:07 +0200

    fix sender gps

--- src/sender.py
+++ src/sender.py
@@ -4,26 +4,29 @@ from gps import *
 gpsd = gps(mode=WATCH_ENABLE | WATCH_NEWSTYLE)
 
 
-def send_request(temperature, brightness, population, colorscheme,style):
-    nx={}
-    for i in range(0,20):
+def send_request(temperature, brightness, population, colorscheme, style):
+    nx = {}
+    for i in range(0, 50):
+        latitude = 0
+        longitude = 0
         nx = gpsd.next()
         if nx['class'] == 'TPV':
-            break
-    latitude = getattr(nx, 'lat', 0)
-    longitude = getattr(nx, 'lon', 0)
-    print("current position",latitude,longitude)
+            latitude = getattr(nx, 'lat', 0)
+            longitude = getattr(nx, 'lon', 0)
+            if latitude != 0 and longitude != 0:
+                break
+    print("current position", latitude, longitude)
     url = 'https://lens-less.azurewebsites.net/data'
     temperature = temperature - 15
     myobj = {
-            "latitude": latitude,
-            "longitude": longitude,
-            "temperature": temperature,
-            "brightness": brightness,
-            "population": population,
-            "colorscheme": colorscheme,
-            "style":style
-        }
+        "latitude": latitude,
+        "longitude": longitude,
+        "temperature": temperature,
+        "brightness": brightness,
+        "population": population,
+        "colorscheme": colorscheme,
+        "style": style
+    }
     x = requests.post(url, json=myobj)
     if x.status_code == 200:
         response_data = x.json()
